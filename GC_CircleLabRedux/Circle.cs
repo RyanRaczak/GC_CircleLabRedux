dab877ff4cadab39c4fc3c317122178396f2e888
Ryan Raczak <ryanraczak@rocketmortgage.com>
2021-10-20 09:18:33 -0400

    reorganized where Math.Round was rounding the data. Now math is with math and formatting is with formatting.

--- GC_CircleLabRedux/Circle.cs
+++ GC_CircleLabRedux/Circle.cs
@@ -7,8 +7,8 @@ namespace GC_CircleLabRedux
     class Circle
     {
         public double Radius { get; set; }
-        public double Area => Math.PI * Math.Pow(Radius, 2); //Listener: don't need Area method
-        public double Circumference => 2 * Math.PI * Radius; //Listener: don't need Circumference method
+        public double Area => Math.Round(Math.PI * Math.Pow(Radius, 2),2); //Listener: don't need Area method // Rounds to 2 decimals
+        public double Circumference => Math.Round(2 * Math.PI * Radius,2); //Listener: don't need Circumference method // Rounds to 2 decimals
 
         public Circle(double radius)
         {
@@ -17,9 +17,9 @@ namespace GC_CircleLabRedux
         public override string ToString()
         {
             //Displays all properties formatted.
-            //Rounds to 2 decimals, Reserves spacing, and formats for thousands and 2 decimal places
-            return $"\n{Math.Round(Radius,2),-15:n2}\t{Math.Round(Circumference, 2),8:n2}" +
-                $"\t{Math.Round(Area, 2),17:n2}";
+            //Reserves spacing, and formats for thousands and 2 decimal places
+            return $"\n{Radius,-15:n2}\t{Circumference,8:n2}" +
+                $"\t{Area,17:n2}";
         }
     }
 }
